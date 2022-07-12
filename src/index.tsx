22ec5d2fe5b0eb92e357be98115c2c6b5de6e9e9
dima0000bf <dkapustin.bf@gmail.com>
2022-07-12 18:26:40 +0400

    improve mobx usage

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,8 @@
-import { autorun } from 'mobx'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import { App } from './App'
 import './index.css'
-import { Test } from './stores/Test'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -15,13 +13,3 @@ root.render(
     <App />
   </React.StrictMode>,
 )
-
-const t = new Test()
-setInterval(() => {
-  t.tick()
-}, 1000)
-
-autorun(() => {
-  console.log(t.kek)
-
-})
